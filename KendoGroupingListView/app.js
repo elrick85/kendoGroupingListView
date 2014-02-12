aefaa1bab3837c6febf2b541e40dd25bed894328
Zaur Ismailov <zaur.ismailov85@gmail.com>
2014-02-12 22:28:50 +0400

    Create project

---
+++ KendoGroupingListView/app.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="js/jquery.min.js" />
+/// <reference path="js/kendo.web.js" />
+
+var App = (function (window, kendo, $) {
+	var crudServiceBaseUrl = "http://demos.telerik.com/kendo-ui/service";
+	    
+	return kendo.Class.extend({
+		model: kendo.observable({
+			data: new kendo.data.DataSource({
+				autoSync: true,
+				transport: {
+					read: {
+						url: crudServiceBaseUrl + "/Products",
+						dataType: "jsonp"
+					},
+					update: {
+						url: crudServiceBaseUrl + "/Products/Update",
+						dataType: "jsonp"
+					},
+					parameterMap: function (options, operation) {
+						if (operation !== "read" && options.models) {
+							return { models: kendo.stringify(options.models) };
+						}
+					}
+				},
+				batch: true,
+				group: { field: "Discontinued" },
+				schema: {
+					model: {
+						id: "ProductID"
+					}
+				}
+			})
+		}),
+		init: function() {
+			var model = this.model;
+			model.data.read();
+			kendo.bind($("#view"), model);
+		}
+	});
+})(window, kendo, kendo.jQuery);
---
+++ KendoGroupingListView/kendo.extends.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="js/jquery.min.js" />
+/// <reference path="js/kendo.web.js" />
+
+kendo_module({
+	id: "groupingListView",
+	name: "GroupingListView",
+	category: "web",
+	description: "Виджет для отображения группированного списка.",
+	depends: ["core"]
+});
+
+(function (kendo, $) {
+	var widget = kendo.ui.ListView;
+	
+	var custom = widget.extend({
+		options: {
+			name: "GroupingListView",
+			groupTemplate: null,
+			rowTemplate: null
+		},
+		init: function (element, options) {
+			var that = this;
+			
+			var groupTemplate = '<div style="width: 350px; float: left">' +
+								'<span>Discontinued: ${value}</span>' +
+								'<div data-template="' + $(element).data("row-template") + '" data-role="listview" data-bind="source: items"></div>' +
+								'</div>';
+			
+			options.template = kendo.template(options.groupTemplate || groupTemplate);
+			widget.fn.init.call(that, element, options);
+			
+			that.element = $(element);
+		}
+	});
+
+	kendo.ui.plugin(custom);
+})(kendo, kendo.jQuery);
